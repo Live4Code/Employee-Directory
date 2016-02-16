1feb8aefc545c21f2774c4474e16b1d973d619ae
Ming He <meticulous.dft@gmail.com>
2016-02-15 18:25:48 -0800

    init

---
+++ data/models/Employee.js
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+
+var EmployeeSchema = new mongoose.Schema({
+  id: {
+    type: String,
+  },
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  title: {
+    type: String
+  },
+  department: {
+    type: String
+  },
+  mobilePhone: {
+    type: String
+  },
+  officePhone: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  city: {
+    type: String
+  },
+  pic: {
+    type: String
+  },
+  twitterId: {
+    type: String
+  },
+  blog: {
+    type: String
+  },
+  reports: {
+    type: Number
+  },
+  manager: {
+    type: Number
+  }
+});
+
+/* take care by graphql
+EmployeeSchema.pre('find', (next) => {
+  this.populate('manager', 'firstName lastName');
+  next();
+});
+
+EmployeeSchema.virtual('managerName').get(() => {
+  return this.manager.firstName + ' ' + this.manager.lastName;
+}); */
+
+EmployeeSchema.index({'$**': 'text'});
+
+var Employee = mongoose.model('Employee', EmployeeSchema);
+
+export default Employee;
---
+++ js/app.js
@@ -0,0 +1,37 @@
+import 'babel-polyfill';
+
+import App from './components/App';
+import EmployeeList from './components/EmployeeList';
+import EmployeePage from './components/EmployeePage';
+//import AppHomeRoute from './routes/AppHomeRoute';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Relay from 'react-relay';
+import createHashHistory from 'history/lib/createHashHistory';
+import { useRouterHistory } from 'react-router';
+import {Route, IndexRoute} from 'react-router';
+import {RelayRouter} from 'react-router-relay';
+
+const history = useRouterHistory(createHashHistory)({ queryKey: false });
+/*
+ReactDOM.render(
+  <Relay.RootContainer
+    Component={App}
+    route={new AppHomeRoute()}
+  />,
+  document.getElementById('root')
+); */
+
+const viewerQueries = {
+  viewer: () => Relay.QL`query { viewer }`,
+};
+
+ReactDOM.render(
+  <RelayRouter history={history}>
+    <Route path="/" component={App} queries={viewerQueries} />
+    <Route path="employees/:employeeId" component={EmployeePage}
+      queries={viewerQueries}
+    />
+  </RelayRouter>,
+  document.getElementById('root')
+);
